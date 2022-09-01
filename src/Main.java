700927c44bb3e940c1415e984183eacbf9ede534
DataGamingYT <rylanhughey@gmail.com>
2022-09-01 09:52:12 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("           . 88888888: .");
+        System.out.println("          88888888.88888 .");
+        System.out.println("         .888888888888888 .");
+        System.out.println("         8888888888888888888");
+        System.out.println("         88' _ `88 '_  `88888");
+        System.out.println("         88  88  88  88  8888");
+        System.out.println("         88:::,:: ,:::::88888");
+        System.out.println("        .88  `;;;;'       8888");
+        System.out.println("       8888               `8:888.");
+        System.out.println("     .8888'               `888888.");
+        System.out.println("    .8888:..   .::.   ...:'8888888:.");
+        System.out.println("   .8888.'      :'      `'::`88:88888");
+        System.out.println("  .8888         '          `.888:88888");
+        System.out.println(" 888:88          .           888:88888");
+    }
+}
