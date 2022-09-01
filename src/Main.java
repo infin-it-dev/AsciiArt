df50a348e37758db6a6b6b9fe7f2c453c9e6a1b9
Rylan Hughey <rylanhughey@gmail.com>
2022-09-01 17:19:50 -0500

    Update Main.java
    finishing touches but github decided to be dumb

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,30 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("            . 88888888: .");
-        System.out.println("           88888888.88888 .");
-        System.out.println("          .888888888888888 .");
-        System.out.println("          8888888888888888888");
-        System.out.println("          88' _ `88 '_  `88888");
-        System.out.println("          88  88  88  88  8888");
-        System.out.println("          88:::,:: ,:::::88888");
-        System.out.println("         .88  `;;;;'       8888");
-        System.out.println("        8888               `8:888.");
-        System.out.println("      .8888'               `888888.");
-        System.out.println("     .8888:..   .::.   ...:'8888888:.");
-        System.out.println("    .8888.'      :'      `'::`88:88888");
-        System.out.println("   .8888         '          `.888:88888");
-        System.out.println("  888:88          .           888:88888");
-        System.out.println(" .888:88         :.           888:88888:");
-        System.out.println(" 8888888.        ::           88:888888");
-        System.out.println(" `.::.888.       ::          .88888888");
-        System.out.println("  .::::::.888.   ::         :::`888'.:.");
-        System.out.println(" :::::::::.888   '         .:8::::::::::::.");
-        System.out.println(" :::::::::::.8    '      .:888:::::::::::::");
-        System.out.println(".::::::::::::::.       .:888:::::::::::::");
-        System.out.println(":::::::::::::::88:.__..:88888:::::::::::");
-        System.out.println(" `'.:::::::::::88888888888.88:::::::::'");
-        System.out.println("       `':::_:'--''
+        System.out.println("              . 88888888 : .                    ");
+        System.out.println("             88888888.8888888 .                  ");
+        System.out.println("            .8888888888888888 .                 ");
+        System.out.println("            8888888888888888888                ");
+        System.out.println("            88' _  88 '_   8888               ");
+        System.out.println("            88  88  88  88  8888               ");
+        System.out.println("            88_88  _: :_88_:8888
+        System.out.println("            88:::,:: ,:::::88888               ");
+        System.out.println("           .88     `;;;;'    `8888              ");
+        System.out.println("          8888                `8:888.           ");
+        System.out.println("        .8888'                `888888.          ");
+        System.out.println("       .8888:..... .::......:'8888888:.        ");
+        System.out.println("      .8888.'       :'      `'::`88:88888       ");
+        System.out.println("     .8888          '          `.888:88888      ");
+        System.out.println("    888:88           .           888:88888      ");
+        System.out.println("   .888:88          :.           888:88888:     ");
+        System.out.println("   8888888.         ::           88:888888      ");
+        System.out.println("   `.::.888.        ::          .88888888       ");
+        System.out.println("    .::::::.888.    ::         :::`888'.:.      ");
+        System.out.println("   :::::::::.888    '         .:8::::::::::::.  ");
+        System.out.println("   :::::::::::.8     '      .:888:::::::::::::  ");
+        System.out.println("  .::::::::::::::.       .:888:::::::::::::    ");
+        System.out.println("  :::::::::::::::88:.__..:88888:::::::::::     ");
+        System.out.println("   `'.:::::::::::88888888888.88:::::::::'      ");
+        System.out.println("         `':::_:'--'' -'-' `':_::::'           ");
     }
 }
